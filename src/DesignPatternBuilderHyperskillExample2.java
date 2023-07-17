d9a052be019810a1ae4b952d58c81a9a9bc6f0b0
pwgtiluser.email <87475830+pwgtil@users.noreply.github.com>
2023-07-17 13:20:22 +0200

    feat: implement Builder design pattern - new example from Hyperskill. With Director class, close to the template.

--- src/DesignPatternBuilderHyperskillExample2.java
+++ src/DesignPatternBuilderHyperskillExample2.java
@@ -1,17 +1,27 @@
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+/**
+ * Builder interface describe step of object creation.
+ **/
+interface Builder {
+    Builder setType(String type);
+
+    Builder setLanguages(List<String> languages);
+
+    Builder setExperience(int experience);
+}
 
 /**
  * ConcreteComponent - Geek.
  **/
 class Geek {
 
-    private String type;
-    private List<String> languages;
-    private int experience;
+    private final String type;
+    private final List<String> languages;
+    private final int experience;
 
-    public Geek(String type, List<String> languages, int experience) {
+    Geek(String type, List<String> languages, int experience) {
         this.type = type;
         this.languages = languages;
         this.experience = experience;
@@ -19,20 +29,11 @@ class Geek {
 
     @Override
     public String toString() {
-        return "Type : " + type + "\n" + "Languages : " + languages + "\n" + "Experience : " + experience + " years";
+        return "Type : " + type + "\n" +
+                "Languages : " + languages + "\n" +
+                "Experience : " + experience + " years";
     }
 
-}
-
-/**
- * Builder interface describe step of object creation.
- **/
-interface Builder {
-    void setType(String type);
-
-    void setLanguages(List<String> languages);
-
-    void setExperience(int experience);
 }
 
 /**
@@ -45,22 +46,25 @@ class GeekBuilder implements Builder {
     private int experience;
 
     @Override
-    public void setType(String type) {
+    public GeekBuilder setType(String type) {
         this.type = type;
+        return this;
     }
 
     @Override
-    public void setLanguages(List<String> languages) {
+    public GeekBuilder setLanguages(List<String> languages) {
         this.languages = languages;
+        return this;
     }
 
     @Override
-    public void setExperience(int experience) {
+    public GeekBuilder setExperience(int experience) {
         this.experience = experience;
+        return this;
     }
 
     public Geek getResult() {
-        return new Geek(type, languages, experience);
+        return new Geek(this.type, this.languages, this.experience);
     }
 }
 
@@ -68,26 +72,67 @@ class GeekBuilder implements Builder {
  * Builder Director.
  */
 class GeekDirector {
-    public void buildAdmin(Builder builder) {
-        builder.setType("Admin");
-        ArrayList<String> languages = new ArrayList<>();
-        languages.add("Perl");
-        languages.add("PowerShell");
-        builder.setLanguages(languages);
-        builder.setExperience(10);
+    private final Builder builder;
+
+    public GeekDirector(Builder builder) {
+        this.builder = builder;
+    }
+
+    public void buildAdmin() {
+        this.builder
+                .setType("Admin")
+                .setLanguages(List.of("Perl", "PowerShell"))
+                .setExperience(10);
+    }
+
+    public void buildBackend() {
+        this.builder.
+                setType("Backend").
+                setLanguages(List.of("Python", "PHP")).
+                setExperience(5);
+    }
+
+    public void buildRockstar() {
+        this.builder.
+                setType("Rockstar").
+                setLanguages(List.of("Java", "Kotlin", "Scala", "Angular")).
+                setExperience(20);
     }
 }
 
 class Main {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+
         final String geekName = scanner.nextLine();
+        final String geekType = scanner.nextLine();
+
         scanner.close();
-        System.out.println("Geek " + geekName + " created.");
-        GeekDirector director = new GeekDirector();
+
         GeekBuilder builder = new GeekBuilder();
-        director.buildAdmin(builder);
-        Geek geek = builder.getResult();
+        GeekDirector director = new GeekDirector(builder);
+        Geek geek;
+
+        switch (geekType) {
+            case "Rockstar" -> {
+                director.buildRockstar();
+                geek = builder.getResult();
+            }
+            case "Backend" -> {
+                director.buildBackend();
+                geek = builder.getResult();
+            }
+            case "Admin" -> {
+                director.buildAdmin();
+                geek = builder.getResult();
+            }
+            default -> {
+                System.out.println("Error");
+                return;
+            }
+        }
+
+        System.out.println("Geek " + geekName + " created.");
         System.out.println(geek);
     }
 }
