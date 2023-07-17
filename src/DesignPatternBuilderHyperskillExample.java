b7778e76c3e96645786a8c2ac3c24cda9df2ad33
pwgtiluser.email <87475830+pwgtil@users.noreply.github.com>
2023-07-17 13:07:45 +0200

    feat: implement Builder design pattern - new example from Hyperskill. With usage of inner class and private constructor

---
+++ src/DesignPatternBuilderHyperskillExample.java
@@ -0,0 +1,91 @@
+class TestDrive {
+    public static void main(String[] args) {
+
+        AlertDialog twoButtonsDialog = new AlertDialog.Builder()
+                .setTitle("Two buttons dialog")
+                .setText("You can use either `Okay` or `Cancel`")
+                .setApplyButton("Okay")
+                .setCancelButton("Cancel")
+                .build();
+
+        System.out.println(twoButtonsDialog);
+
+        AlertDialog oneButtonsDialog = new AlertDialog.Builder()
+                .setTitle("One button dialog")
+                .setText("You can use `Close` only")
+                .setCancelButton("Close")
+                .build();
+
+        System.out.println(oneButtonsDialog);
+    }
+}
+
+
+class AlertDialog {
+    private String title;
+    private String text;
+    private String applyButton;
+    private String cancelButton;
+
+    private AlertDialog(String title, String text, String applyButton, String cancelButton) {
+        this.title = title;
+        this.text = text;
+        this.applyButton = applyButton;
+        this.cancelButton = cancelButton;
+    }
+
+    @Override
+    public String toString() {
+        String str = "";
+        if (title != null) {
+            str += "The title is: \"" + title + "\"\n";
+        }
+        if (text != null) {
+            str += "The text is: \"" + text + "\"\n";
+        }
+        if (applyButton != null) {
+            str += "The applyButton is: \"" + applyButton + "\"\n";
+        }
+        if (cancelButton != null) {
+            str += "The cancelButton is: \"" + cancelButton + "\"\n";
+        }
+
+        return str;
+    }
+
+    /*
+    * Inner class access
+    * */
+    static class Builder {
+        private String title;
+        private String text;
+        private String applyButton;
+        private String cancelButton;
+
+        Builder() {}
+
+        Builder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        Builder setText(String text) {
+            this.text = text;
+            return this;
+        }
+
+        Builder setApplyButton(String applyButton) {
+            this.applyButton = applyButton;
+            return this;
+        }
+
+        Builder setCancelButton(String cancelButton) {
+            this.cancelButton = cancelButton;
+            return this;
+        }
+
+        AlertDialog build() {
+            return new AlertDialog(title, text, applyButton, cancelButton);
+        }
+    }
+}
